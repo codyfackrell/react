40bf7d462e4a2f534f7d58534aae5659d4b19a73
Cody Fackrell <codyfackrell56@gmail.com>
2025-10-10 23:09:21 -0600

    added pages folder, update components

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import MovieCard from "../components/MovieCard";
+
+function Home() {
+  const movies = [
+    { id: 1, title: "John Wick", release_date: "2020" },
+    { id: 2, title: "Talledega Nights", release_date: "2003" },
+    { id: 3, title: "Trolls", release_date: "2014" },
+  ];
+  return (
+    <div className="home">
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
