465819b2a0bc37d7cc7049aa7882ca52f0da5247
Cody Fackrell <codyfackrell56@gmail.com>
2025-10-14 22:29:31 -0600

    updated search and loading screens

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -21,8 +21,7 @@ function MovieCard({ movie }) {
 
       <div className="movie-info">
         <h3>{movie.title}</h3>
-        <p>{movie.release_date?.split("-")[0]}</p>
-        {/* Above is to isolate the year from the full date */}
+        <p>{movie.release_date?.split("-")[0]}</p>{" "}
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,9 +24,23 @@ function Home() {
     loadPopularMovies();
   }, []);
 
-  const handleSearch = (e) => {
+  const handleSearch = async (e) => {
     e.preventDefault();
-    alert(searchQuery);
+    if (!searchQuery.trim()) return;
+    if (loading) return;
+    setLoading(true);
+    try {
+      const searchResults = await searchMovies(searchQuery);
+      setMovies(searchResults);
+      setError(null);
+    } catch (err) {
+      setError("Failed to search movies..");
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+
+    setSearchQuery("");
   };
 
   return (
@@ -44,11 +58,17 @@ function Home() {
         </button>
       </form>
 
-      <div className="movies-grid">
-        {movies.map((movie) => (
-          <MovieCard movie={movie} key={movie.id} />
-        ))}
-      </div>
+      {error && <div className="error-message">{error}</div>}
+
+      {loading ? (
+        <div className="loading">Loading...</div>
+      ) : (
+        <div className="movies-grid">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
