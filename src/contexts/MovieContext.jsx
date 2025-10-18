1805bce03af0cad5ad6aeb3f50ae5004d2b1b1c7
Cody Fackrell <codyfackrell56@gmail.com>
2025-10-17 22:18:06 -0600

    react context

--- src/contexts/MovieContext.jsx
+++ src/contexts/MovieContext.jsx
@@ -1,3 +1,11 @@
 import { useState, useEffect, createContext, useContext } from "react";
 
 const MovieContext = createContext();
+
+export const useMovieContext = () => useContext(MovieContext);
+
+// the provider will provide state to any of the components that are wrapped inside of it.
+// children is a reserved prop when you write a component. Children is anything thats inside the component that you rendered.
+export const MovieProvider = ({ children }) => {
+  return <MovieContext.Provider></MovieContext.Provider>;
+};
