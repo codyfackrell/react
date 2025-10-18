d0064976c390bf44b5a553590433197b20026b22
Cody Fackrell <codyfackrell56@gmail.com>
2025-10-18 06:57:41 -0600

    react context

--- src/contexts/MovieContext.jsx
+++ src/contexts/MovieContext.jsx
@@ -13,7 +13,23 @@ export const MovieProvider = ({ children }) => {
     const storedFavs = localStorage.getItem("favorites");
 
     if (storedFavs) setFavorites(JSON.parse(storedFavs));
-  });
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }, [favorites]);
+
+  const addToFavorites = (movie) => {
+    setFavorites((prev) => [...prev, movie]);
+  };
+
+  const removieFromFavorites = (movieId) => {
+    setFavorites((prev) => prev.filter((movie) => movie.id !== movieId));
+  };
+
+  const isFavorite = (movieId) => {
+    return favorites.some((movie) => movie.id === movieId);
+  };
 
   return <MovieContext.Provider>{children}</MovieContext.Provider>;
 };
