a947e639a323d56941944d3874508ab41e5ef2d7
Cody Fackrell <codyfackrell56@gmail.com>
2025-10-17 22:23:49 -0600

    react context

--- src/contexts/MovieContext.jsx
+++ src/contexts/MovieContext.jsx
@@ -7,5 +7,13 @@ export const useMovieContext = () => useContext(MovieContext);
 // the provider will provide state to any of the components that are wrapped inside of it.
 // children is a reserved prop when you write a component. Children is anything thats inside the component that you rendered.
 export const MovieProvider = ({ children }) => {
-  return <MovieContext.Provider></MovieContext.Provider>;
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    const storedFavs = localStorage.getItem("favorites");
+
+    if (storedFavs) setFavorites(JSON.parse(storedFavs));
+  });
+
+  return <MovieContext.Provider>{children}</MovieContext.Provider>;
 };
